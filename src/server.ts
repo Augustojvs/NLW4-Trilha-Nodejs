9f8931b41bfe7770f0627b3b4bca1c1142065895
Augusto <augusto_jvs@hotmail.com>
2021-02-24 00:23:05 -0300

    Inicio do banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database'
+import { router } from "./routes"
 
 const app = express();
 
-
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello world" });
-});
-
-app.post("/", (req, res) => {
-  return res.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("server is running"));
 

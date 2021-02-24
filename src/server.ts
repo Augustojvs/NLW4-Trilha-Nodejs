575334226b101c3406331661492992b629558534
Augusto <augusto_jvs@hotmail.com>
2021-02-23 22:23:08 -0300

    criação do projeto

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import express from 'express';
+import './database'
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello world" });
+});
+
+app.post("/", (req, res) => {
+  return res.json({ message: "Os dados foram salvos com sucesso!" });
+});
+
+app.listen(3333, () => console.log("server is running"));
+
